01060f39f793c1157cb7c61bea0fc7ba70099f54
pranalghulep99 <pranalghulep99@gmail.com>
2023-07-25 09:18:34 +0530

    second

--- GreatestNumber.java
+++ GreatestNumber.java
@@ -7,16 +7,8 @@ public class GreatestNumber {
         System.out.println("Num1 = "+num1);
         System.out.println("Num2 = "+num2);
         System.out.println("Num3 = "+num3);
-        int max = num1;
+        double avg=(num1+num2+num3)/3;
 
-        if(num2 > max) {
-            max = num2;
-        }
-
-        if (num3 > max) {
-            max = num3;
-        }
-
-        System.out.println("The greatest number is: " + max);
+        System.out.println("The Average of number is: " + avg);
     }
 }
